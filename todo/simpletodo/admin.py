688b36709c7880d75d354de3e06a9c253590f8fc
Andrey <andreykosov21@gmail.com>
2023-04-02 19:14:12 +0300

    Add panel admin models
    Signed-off-by: Andrey <andreykosov21@gmail.com>

--- todo/simpletodo/admin.py
+++ todo/simpletodo/admin.py
@@ -1,7 +1,31 @@
 from django.contrib import admin
 from .models import *
 
+
 # Register your models here.
-admin.site.register(ToDo)
-admin.site.register(Category)
-admin.site.register(Solution)
+class ToDoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'create_todo', 'is_status')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+    list_editable = ('is_status',)
+    list_filter = ('is_status', 'create_todo')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class SolutionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(ToDo, ToDoAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Solution, SolutionAdmin)
