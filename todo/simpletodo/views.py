9fbf51603121ebe37fc05f5662fdcbecc7a5c428
Andrey <andreykosov21@gmail.com>
2023-04-02 20:16:36 +0300

    Add html page, urls site, classes info
    Signed-off-by: Andrey <andreykosov21@gmail.com>

--- todo/simpletodo/views.py
+++ todo/simpletodo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import *
+
 
 # Create your views here.
+class ToDoHome(ListView):
+    model = ToDo
+    template_name = 'simpletodo/home.html'
+    context_object_name = 'posts'
