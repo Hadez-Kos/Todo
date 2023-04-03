a226bb8cb40f43f8c3aaf3531450555da2c3c7dc
Andrey <andreykosov21@gmail.com>
2023-04-03 22:05:14 +0300

    Refactor html page and enter task on site.
    Signed-off-by: Andrey <andreykosov21@gmail.com>

--- todo/simpletodo/views.py
+++ todo/simpletodo/views.py
@@ -8,3 +8,8 @@ class ToDoHome(ListView):
     model = ToDo
     template_name = 'simpletodo/home.html'
     context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Планировщик дел'
+        return context
