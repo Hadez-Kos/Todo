e8d01a12a6d6d82c453e6650adfd8a99cb710201
Andrey <andreykosov21@gmail.com>
2023-04-08 02:43:20 +0300

    Project TODO site version 1.0
    Signed-off-by: Andrey <andreykosov21@gmail.com>

--- todo/RunProject.py
+++ todo/RunProject.py
@@ -2,6 +2,12 @@ import os
 
 if __name__ == '__main__':
     try:
+        os.system("python manage.py makemigrations")
+        os.system("python manage.py migrate")
+    except Exception as e:
+        pass
+
+    try:
         os.system("python manage.py runserver")
     except KeyboardInterrupt as k:
         print("Stopped program")
