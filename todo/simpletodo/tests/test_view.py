796d862180273ca1e17c26480ccba9d01b5fd170
Andrey <andreykosov21@gmail.com>
2023-04-08 02:28:56 +0300

    Add test, correct create test
    Signed-off-by: Andrey <andreykosov21@gmail.com>

---
+++ todo/simpletodo/tests/test_view.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from simpletodo.models import *
+from django.urls import reverse
+
+
+# Create your tests here.
+
+class ToDoModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        ToDo.objects.create(
+            title="Программирование",
+            content="Научиться fullstack разработке",
+            is_status=True,
+            slug="program",
+            user_id=1,
+            cat_id=4,
+            sol_id=3,
+        )
+
+    def test_first_name_admin(self):
+        resp = ToDo.objects.get(id=10)
+        name_id = resp.user_id
+        self.assertEquals(name_id, "root")
